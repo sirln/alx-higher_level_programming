bf637ca5aa69dd4d9ce95b1892dbb4e714bd1a4d
sirln <siro@sirlawren.com>
2023-06-07 01:06:12 +0000

    Correction of code style to Python - if/else, loops, functions. Task 16

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
     if (a < b):
-	return (a)
+        return (a)
     elif (b > c):
         return (a + b)
     else:
         return (c - (a * b))
-
