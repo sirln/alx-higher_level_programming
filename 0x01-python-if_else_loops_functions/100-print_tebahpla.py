a850e3dcfd4b3643ed2e3752a40aeff135425ba8
sirln <siro@sirlawren.com>
2023-06-07 00:30:04 +0000

    Python - if/else, loops, functions. Task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,7 +3,7 @@ def reverse_alpha():
     for i in range(90, 64, -1):
         if (i % 2) == 0:
             i = i + 32
-        print(f"{chr(i)}", end="")
+        print("{}".format(chr(i)), end="")
 
 
 if __name__ == "__main__":
