af1902f0ec93399c2682b15341d77704985b20e7
sirln <siro@sirlawren.com>
2023-06-08 19:47:20 +0000

    Python - import & modules. Advanced:Task 8

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from magic_calculation_102 import add, sub
+from magic_calculation_102 import add, sub
 
+def magic_calculation(a, b):
     if (a < b):
         c = add(a, b)
 
