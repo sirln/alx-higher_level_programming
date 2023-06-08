4e1138e52f07363396c1727741126a85cfb1056a
sirln <siro@sirlawren.com>
2023-06-08 19:42:35 +0000

    Python - import & modules. Advanced:Task 8

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from magic_calculation_102 import add, sub
+
+    if (a < b):
+        c = add(a, b)
+
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+
+    return (sub(a, b))
