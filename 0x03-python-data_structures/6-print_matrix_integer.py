16aff3dc65810818718f1fefa942e7469904a675
sirln <siro@sirlawren.com>
2023-06-12 13:34:57 +0000

    Python - Data Structures: Lists, Tuples. Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,4 +4,4 @@ def print_matrix_integer(matrix=[[]]):
     for list in matrix:
         for element in list:
             print("{:d}".format(element), end=' ')
-        print()
+        print("".format())
