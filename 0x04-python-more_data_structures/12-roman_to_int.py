e23cbd0dae420939d572ea3515cb8d4d299b1c88
sirln <siro@sirlawren.com>
2023-06-14 21:26:26 +0000

    Python - More Data Structures: Set, Dictionary. Task 12,  converts a Roman numeral to an integer.

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    roman_numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                     'C': 100, 'D': 500, 'M': 1000}
+    result = number = 0
+
+    for c in reversed(roman_string):
+        value = roman_numbers[c]
+        if value >= number:
+            result += value
+        else:
+            result -= value
+
+        number = value
+    return (result)
