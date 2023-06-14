0f9da4b2a23e0c7d6e862fc577a5cef48f9b9bfd
sirln <siro@sirlawren.com>
2023-06-14 12:23:01 +0000

    Python - More Data Structures: Set, Dictionary. Task 0, square of a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+def square(a):
+    return (a * a)
+
+
 def square_matrix_simple(matrix=[]):
-    return ([[a * a for a in row] for row in matrix])
+    # return ([[a * a for a in row] for row in matrix])
+    return (list(map(lambda row: list(map(square, row)), matrix)))
