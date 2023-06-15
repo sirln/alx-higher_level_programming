e602e510a0e961b4feb2b14f60b11ad3981254b9
sirln <siro@sirlawren.com>
2023-06-15 00:11:49 +0000

    Python - More Data Structures: Set, Dictionary. Task 16, print some basic info about Python lists and Python bytes objects.

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,26 +1,34 @@
 #include <Python.h>
 #include <stdio.h>
 
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+
 /**
-  *print_python_list - print a python list
+  *print_python_list - print a python list info
   *
   *@p: python object
  */
 void print_python_list(PyObject *p)
 {
-	Py_ssize_t size = PyObject_Length(p);
-	Py_ssize_t i;
-	PyObject *item;
+	int size, i, ;
 	const char *type_name;
+	PyVarObject *item;
+	PyListObject *list = (PyListObject *)p;
+
+	item = (PyVarObject *)p;
+	size = item->ob_size;
 
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", list->allocated);
 
 	for (i = 0; i < size; i++)
 	{
-		item = PyList_GetItem(p, i);
-		type_name = item->ob_type->tp_name;
+		type_name = list->ob_item[i]->ob_type->tp_name;
 		printf("Element %ld: %s\n", i, type_name);
+		if (strcmp(type_name, "bytes") == 0):
+			print_python_bytes(list->ob_item[i]);
 	}
 }
 
@@ -31,29 +39,31 @@ void print_python_list(PyObject *p)
  */
 void print_python_bytes(PyObject *p)
 {
-	Py_ssize_t size;
-	char *bytes_str;
-	Py_ssize_t i;
+	unsigned char i, size;
+	PyBytesObject *bytes_str;
 
 	printf("[.] bytes object info\n");
-	if (PyObject_HasAttrString(p, "encode"))
+	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return (0);
 	}
 
-	size = PyObject_Length(p);
-	bytes_str = PyUnicode_AsUTF8(p);
+	size = ((PyVarObject *)p)->ob_size;
+	bytes_str = ((PyListObject *)p)->ob_sval;
 
 	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", bytes_str);
 
 	if (size > 10)
 		size = 10;
+	else
+		size += 1;
 
 	printf("  first %ld bytes: ", size);
 	for (i = 0; i < size; i++)
 	{
-		printf("%02x", (unsigned char)bytes_str[i]);
+		printf("%02x", bytes_str[i]);
 		if (i < size - 1)
 			printf(" ");
 	}
