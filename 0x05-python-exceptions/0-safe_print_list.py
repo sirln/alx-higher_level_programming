9783542338bb654fc1646766ccbdaddc2c079f1a
sirln <siro@sirlawren.com>
2023-06-26 20:53:02 +0000

    Python - Exceptions, Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         i = 0
         while (i < x):
-            print("{:d}".format(my_list[i]), end="")
+            print("{}".format(my_list[i]), end="")
             i += 1
         print("")
         return (i)
