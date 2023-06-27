29cc1bfb1a31512bb45b9d7def426268e5253eff
sirln <siro@sirlawren.com>
2023-06-27 22:29:11 +0000

    Python - Classes and Objects; adding 3-square.py file for Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+'''
+Square class module
+'''
+
+
+class Square:
+    '''
+    Square class
+    '''
+    def __init__(self, size=0):
+        '''
+        private square instance initialization
+
+        Arguments
+        ---------
+        size : int
+            size of the square
+
+        Raises
+        ------
+        TypeError
+            if argument(size) is not an integer.
+
+        ValueError
+            if argument(size) is less than 0.
+
+        Methods
+        -------
+        area()
+            returns the current square area
+        '''
+        if isinstance(size, int) is False:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        '''
+        calculates and returns the current area of a square
+
+        Returns
+        -------
+        int
+            area of the square
+        '''
+        return (self.__size ** 2)
