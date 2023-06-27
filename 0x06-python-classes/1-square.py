73f1ee6041f6781bd6ae09c91e16f9c475648e0e
sirln <siro@sirlawren.com>
2023-06-27 21:24:22 +0000

    Python - Classes and Objects; adding 1-square.py file for Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+'''
+Square class module
+'''
+
+
+class Square:
+    '''
+    Square class
+    '''
+    def __init__(self, size):
+        '''
+        private square instance initialization
+
+        Arguments
+        ---------
+        size : int
+            size of the square
+        '''
+        self.__size = size
