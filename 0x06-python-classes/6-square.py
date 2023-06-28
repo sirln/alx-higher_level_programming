86b3880676c3d6b3864bebb890933721ac6bbe7a
sirln <siro@sirlawren.com>
2023-06-28 00:32:45 +0000

    Python - Classes and Objects; adding 6-square.py file for Task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -153,8 +153,8 @@ class Square:
         '''
         if self.__size == 0:
             print()
-        else:
-            for p in range(self.__position[1]):
-                print()
-            for s in range(self.__size):
-                print(" " * self.__position[0] + '#' * self.__size)
+
+        for p in range(self.__position[1]):
+            print()
+        for s in range(self.__size):
+            print(" " * self.__position[0] + '#' * self.__size)
