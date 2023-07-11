7e25566c08099bbcd6c9bc5946482c55f6f6af3b
sirln <siro@sirlawren.com>
2023-07-11 00:55:28 +0000

    Python - Inheritance. Task 9. Full rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -29,7 +29,19 @@ class Rectangle(BaseGeometry):
             If width or height is less than or equal to 0.
 
         '''
+        super().integer_validator('width', width)
+        super().integer_validator('height', height)
         self.__width = width
         self.__height = height
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+
+    def area(self):
+        '''
+        Returns the area of a rectangle
+        '''
+        return self.__width * self.__height
+
+    def __str__(self):
+        '''
+        prints the rectangle description
+        '''
+        return (f"[Rectangle] {self.__width:d}/{self.__height:d}")
