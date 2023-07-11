3ec2deedc48312ea6b7caf77c6eb12b7a05bfac4
sirln <siro@sirlawren.com>
2023-07-11 00:28:01 +0000

    Python - Inheritance. Task 7. Integer validator

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -48,7 +48,7 @@ class BaseGeometry():
             If the value is less than or equal to 0.
 
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
