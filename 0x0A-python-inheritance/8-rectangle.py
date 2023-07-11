746e62209908ad3fb6b017fdb500fc8a8f52ce66
sirln <siro@sirlawren.com>
2023-07-11 00:42:12 +0000

    Python - Inheritance. Task 8. Rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,57 +1,9 @@
 #!/usr/bin/python3
 
 '''
-BaseGeometry class module
+Rectangle class module
 '''
-
-
-class BaseGeometry():
-    '''
-    BaseGeometry class.
-
-    An empty class.
-    '''
-
-    def __init__(self):
-        '''
-        Initializing BaseGeometry class objects
-        '''
-        pass
-
-    def area(self):
-        '''
-        calculates the area of the geometry
-
-        Raises
-        ------
-        Exception
-            method should be implemented in derived class
-        '''
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validates the value as an integer.
-
-        Arguments
-        ---------
-        name : str
-            The name of the value.
-        value : int
-            The value to be validated.
-
-        Raises
-        ------
-        TypeError
-            If the value is not an integer.
-        ValueError
-            If the value is less than or equal to 0.
-
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -81,15 +33,3 @@ class Rectangle(BaseGeometry):
         self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-
-    def area(self):
-        """
-        Calculates the area of a rectangle.
-
-        Returns
-        -------
-        int
-            Area of the rectangle.
-
-        """
-        return (self.__width * self.__height)
