e8312a9c5d6259c9924416e90b77aa1ae5ef45e0
sirln <siro@sirlawren.com>
2023-07-11 23:36:25 +0000

    Python - Input/Output. Task 11. Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+
+'''
+Student class module
+'''
+
+
+class Student:
+    '''
+    Class that defines a student
+    '''
+
+    def __init__(self, first_name, last_name, age):
+        '''
+        Student object instance initialization
+
+        Arguments
+        ----------
+        first_name : str
+            Student first name
+        last_name : str
+            Student last name
+        age : int
+            Student age
+
+        Methods
+        -------
+        to_json()
+            retrieves a dictionary representation of a Student instance
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        '''
+        Get a dictionary representation of the Student instance
+
+        Returns
+        -------
+        dict
+            A dictionary containing the attribute names
+            and their corresponding values.
+        '''
+        return (self.__dict__)
+
+    def to_json(self, attrs=None):
+        '''
+        Gets a dictionary representation of a Student instance
+
+        Arguments
+        ---------
+        attrs: (list, optional)
+            list of attributes to get (default is None)
+
+        Returns
+        -------
+        dict
+            Attribute names if attrs is a list
+            Otherwise, all attributes
+        '''
+        if attrs is None:
+            return (self.__dict__)
+        elif not isinstance(attrs, list):
+            return (self.__dict__)
+        else:
+            return ({a: getattr(self, a) for a in attrs if hasattr(self, a)})
+
+    def reload_from_json(self, json):
+        '''
+        Replaces all attributes of the Student instance
+
+        Arguments
+        ---------
+        json: dict
+            A dictionary containing the attribute names
+            and their corresponding values to replace attributes with.
+        '''
+        self.__dict__ = json
