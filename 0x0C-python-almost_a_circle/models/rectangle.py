8d060ff3ffb4701b9ff8648832416ecdddcc4d1f
sirln <siro@sirlawren.com>
2023-07-18 02:07:12 +0000

    Python - Almost a circle. Adding modified models directory

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -238,10 +238,8 @@ class Rectangle(Base):
             in the format:
                 [Rectangle] (<id>) <x>/<y> - <width>/<height>
         '''
-        return (
-            f"[Rectangle] ({self.id}), {self.x}/{self.y} - "
-            f"{self.width}/{self.height}"
-        )
+        return ("[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format
+                (self.id, self.__x, self.__y, self.__width, self.__height))
 
     def update(self, *args, **kwargs):
         '''
