9b2e1eff54300d672491d136191922d9b75ecd3f
sirln <siro@sirlawren.com>
2023-09-12 03:01:54 +0300

    JavaScript - Warm up. Task files

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,8 +2,7 @@
 // script that prints from an array using a loop
 
 const words = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let index;
 
-for (index = 0; index < words.length; index++) {
+for (let index = 0; index < words.length; index++) {
   console.log(words[index]);
 }
