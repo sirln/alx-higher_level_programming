8fbbd8443ccd699fb9493c3b82e88a2b21c8c44e
sirln <siro@sirlawren.com>
2023-09-18 23:29:33 +0300

    Python - Object-relational mapping. Adding Task 8 file, First state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''
+Module to lists all State objects from the database hbtn_0e_6_usa
+'''
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    # Connectivity to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database))
+
+    # Bind the engine to the metadata of the Base class
+    Base.metadata.bind = engine
+
+    # Create a session
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    # Query and print the first State object
+    state = session.query(State).first()
+    if state:
+        print(f'{state.id}: {state.name}')
+    else:
+        print('Nothing')
+    # Close the session
+    session.close()
