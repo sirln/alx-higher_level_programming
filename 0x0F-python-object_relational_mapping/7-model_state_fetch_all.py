1daa7e368cb8767add0915c26163c7776119379e
sirln <siro@sirlawren.com>
2023-09-18 18:05:41 +0300

    Python - Object-relational mapping. Adding Task 7 file, SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,11 +12,11 @@ if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(argv[1], argv[2], argv[3]))
 
-    Base.metadata.create_all(engine)
+    # Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id).all():
+    for state in session.query(State).order_by(State.id):
         print(f'{state.id}:{state.name}')
     session.close()
