45d356d90b4a12b712501f0199ab9cd9eecb0768
sirln <siro@sirlawren.com>
2023-09-18 21:50:49 +0300

    Python - Object-relational mapping. Adding Task 7 file, SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
 '''
-Module to lists all State objects
-from the database hbtn_0e_6_usa
+Module to lists all State objects from the database hbtn_0e_6_usa
 '''
-
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
+if __name__ == '__main__':
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    # Connectivity to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database))
+
+    # Bind the engine to the metadata of the Base class
+    Base.metadata.bind = engine
+
+    # Create a session
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    # Query and print the State objects
+    for state in session.query(State).order_by(State.id):
+        print(f'{state.id}: {state.name}')
+
+    # Close the session
     session.close()
