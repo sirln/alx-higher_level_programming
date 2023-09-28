a60af82fc7529df259472947f57df0a4a46fc549
sirln <siro@sirlawren.com>
2023-09-29 01:19:28 +0300

    Python - Network #0. Uploading `4-header.sh` script

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -sH X-School-User-I:98 -X GET "$1"
+curl -sH X-School-User-Id:98 -X GET "$1"
