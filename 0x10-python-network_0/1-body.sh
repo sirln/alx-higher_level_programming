76fe2c9907a43d66e65db110574540e6803278e2
sirln <siro@sirlawren.com>
2023-09-29 00:47:44 +0300

    Python - Network #0, corrections

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# Script that takes in a URL, sends a `GET` request to the URL,
-# and displays the body of the response
-
-URL=$1
-STATUS_CODE=$(curl -o /dev/null -s -w '%{http_code}' "$URL")
-
-if [ "$STATUS_CODE" -eq 200 ]; then
-	curl -sLX GET "$URL"
-fi
+# Script that takes in a URL, sends a `GET` request to the URL, and displays the body of the response
+curl -sLX GET "$1"
