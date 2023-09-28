eec55ed5d2fa680c4bc55d361092990811ca779d
sirln <siro@sirlawren.com>
2023-09-29 01:27:49 +0300

    Python - Network #0. Uploading `5-post_params.sh` script

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -s -d email:test@gmail.com&subject=I will always be here for PLD -X POST "$1"
+curl -s -d "email:test@gmail.com&subject=I will always be here for PLD" -X POST "$1"
