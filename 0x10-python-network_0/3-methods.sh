9b704592b673a3af285f650c6df95d3aefdb8b05
sirln <siro@sirlawren.com>
2023-09-29 01:09:27 +0300

    Python - Network #0. Uploading `3-methods.sh` script

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -sI -X OPTIONS "$1" | grep -i 'Allow' | cut -f2 -d' '
+curl -sI -X OPTIONS "$1" | grep -w 'Allow' | cut -f2- -d' '
