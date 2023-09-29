0bbf19fd72e872df67a66db7a54755a810604583
sirln <siro@sirlawren.com>
2023-09-29 18:09:20 +0300

    Python - Network #1. Script to get Error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+module that takes in a URL,
+sends a request to the URL
+and displays the body of the response (decoded in utf-8).
+'''
+from sys import argv
+from urllib.request import urlopen
+from urllib.error import HTTPError
+
+
+if __name__ == '__main__':
+    url = argv[1]
+    try:
+        with urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except HTTPError as error:
+        print(f'Error Code: {error.code}')
