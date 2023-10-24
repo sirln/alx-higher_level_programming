1342d1b46db85d40b8838a060c5880b9e9955203
sirln <siro@sirlawren.com>
2023-10-25 01:31:40 +0300

    JavaScript - Web scraping, uploading script to write a string into a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,14 +2,14 @@
 
 const fs = require('fs');
 const filePath = process.argv[2];
-const contentToWrite = process.argv[3];
+const string = process.argv[3];
 
 if (process.argv.length <= 3) {
   console.error('Provide file path and a string to write.');
   process.exit(1);
 }
 
-fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
+fs.writeFile(filePath, string, 'utf-8', (err) => {
   if (err) {
     console.error(err);
   } else {
