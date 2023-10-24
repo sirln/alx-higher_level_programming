aee1311372ff14ebc4bf034f288e1bfbc556707b
sirln <siro@sirlawren.com>
2023-10-25 01:05:53 +0300

    JavaScript - Web scraping, uploading script to read and print file content

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filePath = process.argv[2];
+
+if (process.argv.length <= 2) {
+    console.error('Provide file path.');
+    process.exit(1);
+}
+
+
+fs.readFile(filePath, 'utf-8', (err, data) =>{
+    if (err) {
+        console.error(err);
+    }
+    else {
+        console.log(data);
+    }
+});
