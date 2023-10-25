2aab51ea37f5cebddaee7b2d2a7385644f65bb2a
sirln <siro@sirlawren.com>
2023-10-25 03:28:25 +0300

    JavaScript - Web scraping, uploading script to filter through movies

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length <= 2) {
+  console.error('Provide API URL...');
+  process.exit(1);
+}
+
+const apiUrl = process.argv[2];
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const films = JSON.parse(body).results;
+  if (!films) {
+    console.error(error);
+    return;
+  }
+  const uri = 'https://swapi-api.alx-tools.com/api/people/18/';
+  const count = films.filter(film => film.characters.includes(`${uri}`));
+  console.log(count.length);
+});
