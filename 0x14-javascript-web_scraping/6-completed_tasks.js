f7a0734066a7f38854ff09c26c05131ea71b73f0
sirln <siro@sirlawren.com>
2023-10-25 04:21:14 +0300

    JavaScript - Web scraping, uploading script to get number of completed tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/snap/bin/node
+
+const request = require('request');
+const apiUrl = process.argv[2];
+
+if (process.argv.length <= 2) {
+  console.error('Provide API url...');
+  process.exit(1);
+}
+
+request.get(apiUrl, (error, response, body) => {
+  const todos = JSON.parse(body);
+  const completedTasks = {};
+  if (error) {
+    console.error(error);
+  }
+
+  todos.forEach(task => {
+    if (task.completed) {
+      completedTasks[task.userId] = (completedTasks[task.userId] || 0) + 1;
+    }
+  });
+
+  console.log(completedTasks);
+});
